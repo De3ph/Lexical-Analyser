df2602025fcbc252f569b4d11169346400215250
De3ph <05190000108@ogrenci.ege.edu.tr>
2021-05-21 19:48:42 +0300

    try

--- analyzer.c
+++ analyzer.c
@@ -2,8 +2,23 @@
 #include <stdlib.h>
 #include<string.h>
 
+
 char ENDOFLINE = '.';
 char *KEYWORDS[9] ={"int","move","to","loop","times","out","newline","add","sub"}; 
+
+
+void isValidEnd(char lastChar){
+    // eğer en sonda nokta yoksa hatali statement
+    if (lastChar != ENDOFLINE)
+    {   
+        printf("%s","satir '.' ile bitmiyor.");
+        exit(0);
+    }
+    else{
+        printf("%s","ez");
+    }
+}
+
 
 int main(int argc, char *argv[]) //içteki şeyler cmd de parametre vermeye yariyor
 {
@@ -26,14 +41,17 @@ int main(int argc, char *argv[]) //içteki şeyler cmd de parametre vermeye yari
         strcpy(satirlar[satir_sayisi],satir);
         satir_sayisi++;
     }
+    int anlik_satir = 0;
 
-    printf("%i",satir_sayisi);
-
-    for (int i = 0; i < strlen(satirlar); i++)
+    while (anlik_satir < strlen(*satirlar))
     {
-        puts(satirlar[i]);
+        isValidEnd(satirlar[anlik_satir][strlen(satirlar[anlik_satir])-2]);
+        exit(0);
     }
     
+    
+    
+
     return 0;
 }
 
@@ -52,14 +70,6 @@ int isValidEnd(char lastChar){
 }
 */
 
-void isValidEnd(char lastChar){
-    // eğer en sonda nokta yoksa hatali statement
-    if (lastChar != ENDOFLINE)
-    {   
-        printf("%s","satir '.' ile bitmiyor.");
-        exit(0);
-    }
-}
 
 
 
